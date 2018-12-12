fcd775c58ae6fcdfbbde2184b5db0d7927952e59
Ramón Otero <kaptux@gmail.com>
2018-12-12 20:29:29 +0100

    Fix: avoid innecesary calls to onDataChange

--- src/components/BoardContainer.js
+++ src/components/BoardContainer.js
@@ -31,16 +31,12 @@ class BoardContainer extends Component {
     // nextProps.data changes when external Board input props change and nextProps.reducerData changes due to event bus or UI changes
     const {data, reducerData, onDataChange} = this.props
 
-    console.log('reducerData', reducerData)
-    console.log('next-reducerData', nextProps.reducerData)
-
     if (
-      reducerData.lastAction &&
-      reducerData.lastAction !== 'LOAD_BOARD' &&
       nextProps.reducerData &&
-      !isEqual(reducerData, nextProps.reducerData)
+      nextProps.reducerData.lastAction &&
+      nextProps.reducerData.lastAction !== 'LOAD_BOARD' &&
+      !isEqual(reducerData.lanes, nextProps.reducerData.lanes)
     ) {
-      console.log('--> onDataChange')
       onDataChange(nextProps.reducerData)
     }
 
